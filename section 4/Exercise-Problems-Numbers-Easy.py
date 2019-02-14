1dd5c84ea40e8f0e536f940e474ce066c31a561c
samirsouzasys <samir.guitar@gmail.com>
2019-02-14 07:35:43 -0300

    First momment

---
+++ section 4/Exercise-Problems-Numbers-Easy.py
@@ -0,0 +1,6 @@
+print("Assign 25 to a Integer Variable")
+variableInteger = 0
+print("variableInteger ", variableInteger)
+variableInteger = 25
+print("variableInteger ", variableInteger)
+print("End")
---
+++ section 4/Exercise-Problems-Numbers-Medium.py
@@ -0,0 +1,6 @@
+print("Declare 25.77 as Floating and convert it to a Integer Variable")
+variableFloating = 25.77
+print("variableFloating ", variableFloating)
+variableFloating = int(variableFloating)
+print("variableFloating as Integer ", variableFloating)
+print("End")
